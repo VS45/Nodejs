40cf223fd0e329528ee78b93502c6d0a58f0b15a
vs45 <bringfirehjake88@gmail.com>
2024-06-15 12:55:02 +0100

    removing empty spaces using trim

--- String.js
+++ String.js
@@ -1,6 +1,9 @@
-const myName='Tsenongo Jacob'
+const myName='Tsenongo Jacob '
 
 //length of String
 console.log(myName.length)
 //convert to UPPerCase
 console.log(myName.toLocaleUpperCase())
+console.log(myName.toLocaleLowerCase())
+// removing empty spaces at the end of the value
+console.log(myName.trim().length)
