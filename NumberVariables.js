84378c9d46c196407fb3f7b9a0284389822d19a3
vs45 <bringfirehjake88@gmail.com>
2024-06-15 12:30:55 +0100

    Node.js Tutorials

---
+++ NumberVariables.js
@@ -0,0 +1,4 @@
+const a=45;
+const b=50;
+const add=a+b;
+console.log(add)
---
+++ Variables.js
@@ -0,0 +1,20 @@
+const myName='Tsenongo Jacob';
+console.log(myName)
+
+// Type of variables
+console.log(typeof(myName))
+// data structure (object)
+const Person={name:'Sonter',height:'1.76m',age:31}
+console.log(Person)
+console.log(typeof(Person))
+
+//number datatype
+
+var age=31
+console.log(typeof(age))
+
+//arrays datatype
+
+const fruits=['Mangoe','Orange','Apple','Banana']
+console.log(fruits)
+
