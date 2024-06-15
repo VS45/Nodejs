1d3bdadafafba5591d87b0a93f74cc4150db4f50
vs45 <bringfirehjake88@gmail.com>
2024-06-15 16:51:48 +0100

    map function

--- Arrays.js
+++ Arrays.js
@@ -24,3 +24,26 @@ console.log(fruits)
 
 fruits.splice(1,0, "Coconut");
 console.log(fruits)
+let index = 2;
+let newElements = ['Cashew', 'plantain'];
+
+fruits.splice(index, 0, ...newElements);
+console.log(fruits)
+
+// array of Objects
+
+const persons=[{name:'Steve',age:14},{name:'John', age:20},{name:'Naomi',age:22},{name:'Franca',age:12}]
+console.log(persons.length)
+
+const newPersons=persons.map((person,index)=>{
+    let newPerson;
+    if(person.age<=14){
+        newPerson={...person,status:'not qualified'}
+    }else{
+        newPerson={...person,status:'qualified'}
+    }
+  
+    return newPerson;
+})
+console.log(newPersons)
+//
