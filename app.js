50406ba54fa525298c5bdd325b7abf1b43eb9938
vs45 <bringfirehjake88@gmail.com>
2024-06-22 15:14:26 +0100

    home.ejs file and registering view engine (EJS)

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const appRouter=require('./routes/app.routes')
 const app=express();
 const PORT=8080;
 // set up route for application
+app.set('view engine','ejs')
+app.set('views','frontend')
 app.use(appRouter)
 
 app.listen(PORT,()=>{
